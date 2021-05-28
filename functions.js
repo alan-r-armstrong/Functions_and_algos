fcbf7ec2d31ed8bffa763ce1ec604010bb7ce00a
alan-r-armstrong <alan.armstrong@gmail.com>
2021-05-28 09:23:52 -0700

    daily function predict

--- functions.js
+++ functions.js
@@ -25,3 +25,11 @@ function a(n){
 }
 x = a(3) +a(5);
 console.log('x is', x);
+
+function op(a,b){
+    c = a+b;
+    console.log('c is', c);
+    return c;
+}
+x = op(2,3) + op(3,5);
+console.log('x is', x);
