b970d14b761d854353086939556f2063b60e8cfe
alan-r-armstrong <alan.armstrong@gmail.com>
2021-06-07 15:49:00 -0700

    daily functions

--- functions.js
+++ functions.js
@@ -111,3 +111,174 @@ function foo(x,y){
 }
 var z = foo(3,5);
 console.log(z);
+
+function count(x){
+    for(var i=0; i<x; i += 2){
+    console.log(i);
+    }
+}
+count(255);
+
+for(var a = 1; a <= 100; a++){
+    if(a % 3 == 0){
+        continue;
+    }
+    console.log(a);
+}
+
+let alphabet = "abcdefghijklmnopqrstuvwxyz";
+var greeting = alphabet.substr(7,2);
+console.log(greeting);
+
+function oddsAndEvens(number){
+    var odds = [];
+    var evens = [];
+
+    for(var i = 0; i<=number; i++){
+        if (i % 2 == 0){
+            evens.push(i);
+        }
+        else odds.push(i);
+    }
+    return odds;
+}
+console.log(oddsAndEvens(50));
+
+// Number to String - Write a function that takes an array of numbers and replaces any negative values within the array with the string 'Dojo'. For example if array = [-1,-3,2], your function will return ['Dojo','Dojo',2].
+
+function numtostring(array){
+    var a = "Dojo";
+    for(var i=0; i<array.length; i++){
+        if(array[i] <= -1)
+            array[i] = a;
+        }
+    return array;
+}
+console.log(numtostring([-1,-2,0,-1,2,3,4]));
+
+function a(){
+    return 35;
+}
+console.log(a());
+
+function b(){
+    return 4;
+}
+console.log(b()+b());
+
+function c(b){
+    return b;
+}
+console.log(c(2)+c(4));
+
+function d(b){
+    console.log(b);
+    return b*3;
+}
+console.log(d(3));
+
+function e(b){
+    return b*4;
+    console.log(b);
+}
+console.log(e(10));
+
+function f(b){
+    if(b<10) {
+        return 2;
+    }
+    else     {
+        return 4;
+    }
+    console.log(b);
+}
+console.log(f(15));
+
+function g(b,c){
+    return b*c;
+}
+console.log(10,3);
+console.log( g(3,10) );
+
+function h(b){
+    for(var i=0; i<10; i++){
+        console.log(i);
+    }
+    return i;
+}
+console.log(3);
+console.log(4);
+
+function i(){
+    for(var i=0; i<10; i++){
+        i = i +2;
+        console.log(i);
+    }
+}
+i();
+
+function j(b,c){
+    for(var i=b; i<c; i++) {
+        console.log(i);
+    }
+    return b*c;
+}
+j(0,10);
+console.log(j(0,10));
+
+function k(){
+    for(var i=0; i<10; i++){
+        for(var j=0; j<10; j++){
+            console.log(j);
+        }
+        console.log(i);
+    }
+}
+
+function l(){
+    for(var i=0; i<10; i++){
+        for(var j=0; j<10; j++){
+            console.log(i,j);
+        }
+        console.log(j,i);
+    }
+}
+
+
+var z = 10;
+function m(){
+    var z = 15;
+    console.log(z);
+}
+console.log(z);
+
+var z = 10;
+function n(){
+    var z = 15;
+    console.log(z);
+}
+n();
+console.log(z);
+
+var z = 10;
+function o(){
+    var z = 15;
+    console.log(z);
+    return z;
+}
+z = o();
+console.log(z);
+
+var x = 75;
+if (x > 100) {    
+    console.log("bigger than 100");
+}
+else if (x > 50) {    
+    console.log("bigger than 50");
+}
+else if(x > 25) {
+    console.log("bigger than 25");
+}
+else {    
+    console.log("small number");
+}
